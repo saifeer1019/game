0c164639b3657b4d8728bcab2be25ce66788e557
Saifee Rahman Saif <saifeer1019@gmail.com>
2024-12-21 18:40:50 +0600

    edits

--- src/components/widgets/SearchBar.js
+++ src/components/widgets/SearchBar.js
@@ -1,14 +1,21 @@
 "use client"
 import { useState } from "react";
+import {useRouter} from "next/navigation";
 
 export default function SearchBar() {
     const [search, setSearch] = useState('');
+    const router = useRouter()
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        router.push(`/search/${search}`)
+        
         console.log(search);
     }
+const handleChange = (e) => {
+        setSearch(e.target.value)
 
+}
     return (
        
 <form className="flex items-center justify-between w-[400px] max-w-md mx-auto rounded-lg bg-white py-1 px-2 shadow-lg overflow-hidden" onSubmit={handleSubmit}>   
@@ -19,8 +26,8 @@ export default function SearchBar() {
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
         </svg>
     </div>
-    <input type="search" id="default-search" class=" w-full p-2 text-sm text-gray-900 " placeholder="Search..." required />
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+    <input value={search} onChange={handleChange} type="search" id="default-search" class=" w-full p-2 text-sm text-gray-900 " placeholder="Search..." required />
+    <button onClick={handleSubmit} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
 
 </form>
 
