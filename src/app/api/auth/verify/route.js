5afe6225010098281c89035bd4cccd7609776eba
Saifee Rahman Saif <saifeer1019@gmail.com>
2025-01-22 17:00:40 +0600

    edits

--- src/app/api/auth/verify/route.js
+++ src/app/api/auth/verify/route.js
@@ -1,6 +1,6 @@
 import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
-import { collection,getDoc, getDocs, writeBatch, query, orderBy } from "firebase/firestore";
+import { collection, getDocs, writeBatch, query, orderBy } from "firebase/firestore";
 import { db } from "@/config/firebase";
 
 // Initialize Firebase Admin
@@ -25,22 +25,16 @@ export async function POST(request) {
     const { token } = await request.json();
     const auth = initAdmin();
     const decodedToken = await auth.verifyIdToken(token);
-    const email= decodedToken.email
     
-    const adminQuery = query(admin, where("role", "==", "admin"));
-    const querySnapshot = await getDoc(adminQuery);
-    const emails = querySnapshot.docs.map((doc) => doc.data().email);
-    if(emails.includes(email)){
-      return Response.json({ 
-        user: {
-          uid: decodedToken.uid,
-          email: decodedToken.email
-        }
-      });
-    }
-    else{
-      throw new Error('Not an admin');
-    };
+    
+    const categoryQuery = query(gamesCollectionRef, where(category, "==", true));
+    const querySnapshot = await getDocs(categoryQuery);
+    return Response.json({ 
+      user: {
+        uid: decodedToken.uid,
+        email: decodedToken.email
+      }
+    });
   } catch (error) {
     return Response.json({ error: error.message }, { status: 401 });
   }
